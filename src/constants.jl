3fad9346ba8652b6addf7cba936798f453e56157
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:32:04 +0000

    Snapshot of branch master taken on 2019-04-07T15:32:03.335-04:00 from "https://github.com/UnofficialJuliaMirror/CBOR.jl-7f3e1038-61bc-5414-967e-017c9d82adda"

---
+++ src/constants.jl
@@ -0,0 +1,54 @@
+const TYPE_0 = zero(UInt8)
+const TYPE_1 = one(UInt8) << 5
+const TYPE_2 = UInt8(2) << 5
+const TYPE_3 = UInt8(3) << 5
+const TYPE_4 = UInt8(4) << 5
+const TYPE_5 = UInt8(5) << 5
+const TYPE_6 = UInt8(6) << 5
+const TYPE_7 = UInt8(7) << 5
+
+const BITS_PER_BYTE = UInt8(8)
+const HEX_BASE = Int(16)
+const LOWEST_ORDER_BYTE_MASK = 0xFF
+
+const TYPE_BITS_MASK = UInt8(0b1110_0000)
+const ADDNTL_INFO_MASK = UInt8(0b0001_1111)
+
+const ADDNTL_INFO_UINT8 = UInt8(24)
+const ADDNTL_INFO_UINT16 = UInt8(25)
+const ADDNTL_INFO_UINT32 = UInt8(26)
+const ADDNTL_INFO_UINT64 = UInt8(27)
+
+const SINGLE_BYTE_SIMPLE_PLUS_ONE = UInt8(24)
+const SIMPLE_FALSE = UInt8(20)
+const SIMPLE_TRUE = UInt8(21)
+const SIMPLE_NULL = UInt8(22)
+const SIMPLE_UNDEF = UInt8(23)
+
+const ADDNTL_INFO_FLOAT16 = UInt8(25)
+const ADDNTL_INFO_FLOAT32 = UInt8(26)
+const ADDNTL_INFO_FLOAT64 = UInt8(27)
+
+const ADDNTL_INFO_INDEF = UInt8(31)
+const BREAK_INDEF = TYPE_7 | UInt8(31)
+
+const SINGLE_BYTE_UINT_PLUS_ONE = 24
+const UINT8_MAX_PLUS_ONE = 0x100
+const UINT16_MAX_PLUS_ONE = 0x10000
+const UINT32_MAX_PLUS_ONE = 0x100000000
+const UINT64_MAX_PLUS_ONE = 0x10000000000000000
+
+const SIZE_OF_FLOAT64 = sizeof(Float64)
+const SIZE_OF_FLOAT32 = sizeof(Float32)
+const SIZE_OF_FLOAT16 = sizeof(Float16)
+
+const POS_BIG_INT_TAG = UInt8(2)
+const NEG_BIG_INT_TAG = UInt8(3)
+
+const CBOR_FALSE_BYTE = UInt8(TYPE_7 | 20)
+const CBOR_TRUE_BYTE = UInt8(TYPE_7 | 21)
+const CBOR_NULL_BYTE = UInt8(TYPE_7 | 22)
+const CBOR_UNDEF_BYTE = UInt8(TYPE_7 | 23)
+
+
+const CUSTOM_LANGUAGE_TYPE = 27
